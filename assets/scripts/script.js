a5541672e5f3fe3aaf3c2dd96c91f57e73e76ff8
Lucas Barra <lucastbarra@hotmail.com>
2021-04-16 16:27:17 -0300

    embaralhando as cartasd

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -14,6 +14,32 @@ let techs = [
     'react',
 ];
 
+var cards = null;
+
+startGame();
+
+function startGame() {
+    cards = createCardsFromTechs(techs);
+    shuffleCards(cards);
+    console.log(cards);
+}
+
+function shuffleCards(cards) {
+
+    let currentIndex = cards.length;
+    let randomIndex = 0;
+
+    while (currentIndex !== 0) {
+
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]];
+
+    }
+
+}
+
 createCardsFromTechs(techs);
 function createCardsFromTechs(techs) {
     let cards = [];
